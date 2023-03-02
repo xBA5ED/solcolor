45cf1686fd7cd5e12ca45227fef9dd50f40ade11
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2023-03-03 00:22:13 +0100

    Update test

--- test/Color.t.sol
+++ test/Color.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Color.sol";
+import {Color, LibColor, newColorFromRGBString} from "../src/Color.sol";
 
 contract ColorTest is Test {
     using LibColor for Color;
